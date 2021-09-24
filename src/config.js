8f34ab19dd5f35a809c3793cd0b7a481ac53d148
Ignacio Amatt <ignacio_amatt@hotmail.com>
2021-09-24 11:09:10 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 export const Envoirment = process.env.NODE_ENV
 export const SERVERPORT = process.env.SERVERPORT
 
-export const ApiURL = Envoirment === 'development' ? 'http://localhost:3002' : 'https://noiloan.herokuapp.com/'
+export const ApiURL = Envoirment === 'development' ? 'http://localhost:3002' : 'https://noiloan.herokuapp.com'
